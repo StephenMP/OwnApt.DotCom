4833ed9665f5a524d108f039fef1cb448aec617c
sporter <sporter@ownapt.com>
2016-06-08 20:39:16 -0600

    Moved some things around, changed buttons, added listings

---
+++ src/DotCom/Controllers/ListingsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OwnApt.DotCom.Controllers
+{
+    public class ListingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
