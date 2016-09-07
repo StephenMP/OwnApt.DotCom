b452cea1f6fdef407263423e1590545a18d4b80c
sporter <sporter@ownapt.com>
2016-09-07 08:57:41 -0600

    24 : Added docker url

--- src/DotCom/Program.cs
+++ src/DotCom/Program.cs
@@ -16,6 +16,7 @@ namespace DotCom
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls("https://0.0.0.0:5000")
                 .Build();
 
             host.Run();
