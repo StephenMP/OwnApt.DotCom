3950dd1e5dd4bb6b9112d8fe11e82bac6b807c66
sporter <sporter@ownapt.com>
2016-09-13 22:52:10 -0600

    13 : Added icon images and setup the layout pattern for views

---
+++ src/DotCom/Controllers/OwnerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotCom.Controllers
+{
+    [Authorize]
+    public class OwnerController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return await Task.FromResult(View());
+        }
+    }
+}
